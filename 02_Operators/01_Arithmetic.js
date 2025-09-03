4ea2c5210856f54eb10e9142268ff0d6da071145
PatilShweta12345 <shwetap1507@gmail.com>
2025-09-03 19:13:30 +0530

    first commit

---
+++ 02_Operators/01_Arithmetic.js
@@ -0,0 +1,39 @@
+let a = 10;
+let b = 20;
+
+
+function add(){
+    console.log(a+b);
+}
+
+add();
+
+
+
+let p = 80;
+let q = 50;
+
+function sub(){
+    console.log(p-q);
+}
+
+sub();
+
+let y = 25;
+let z = 5;
+
+function mul(){
+    console.log(y*z);
+
+}
+
+mul();
+
+let m = 50;
+let n = 5;
+
+function div(){
+    console.log(m/n);
+
+}
+div();
